5890a0ff91816234f35dc4ce1719d507486b9dfb
geoffrey-anto <geoffreyanto.ignatiuse2021@vitstudent.ac.in>
2024-09-21 01:57:33 +0530

    added: initial avoidance using HC-SR04

--- obstacle_avoidance/main.py
+++ obstacle_avoidance/main.py
@@ -1,31 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-import utils
+from obstacle_avoidance.utils import UltrasonicSensorsModule
 
 GPIO.setmode(GPIO.BCM)
 
 TRIG_PINS = [23, 24, 25, 26]  
 ECHO_PINS = [27, 28, 29, 30] 
 
-DISTANCE_THRESHOLD = 30 
-
-for trig_pin, echo_pin in zip(TRIG_PINS, ECHO_PINS):
-    GPIO.setup(trig_pin, GPIO.OUT)
-    GPIO.setup(echo_pin, GPIO.IN)
-
-
-
 try:
-    while True:
-        for i, (trig_pin, echo_pin) in enumerate(zip(TRIG_PINS, ECHO_PINS)):
-            distance = utils.measure_distance(trig_pin, echo_pin)
-            direction = ["North", "South", "East", "West"][i]
-            
-            if distance < DISTANCE_THRESHOLD:
-                # speech.speak(f"Obstacle detected {direction}: {distance:.1f} cm away!")
-                print(f"Obstacle detected {direction}: {distance:.1f} cm away!")
-        
-        time.sleep(1)
+    avoidance_module = UltrasonicSensorsModule(TRIG_PINS, ECHO_PINS)
+    
+    avoidance_module.monitor_for_nearby_obstacles()
 except KeyboardInterrupt:
     print("Measurement stopped by User")
     GPIO.cleanup()
--- obstacle_avoidance/utils.py
+++ obstacle_avoidance/utils.py
@@ -1,4 +1,10 @@
 import RPi.GPIO as GPIO
+from typing import List
+import time
+from obstacle_avoidance.config import DISTANCE_THRESHOLD, ABSOLUTE_DISTANCE_THRESHOLD,\
+    SPEED_CALCULATION_INTERVAL, SPEED_ACCEPTANCE_RANGE, SPEED_THRESHOLD
+
+DIRECTION = ["Front", "Back", "Left", "Right"]
 
 def measure_distance(trig_pin, echo_pin):
     GPIO.output(trig_pin, True)
@@ -19,5 +25,87 @@ def measure_distance(trig_pin, echo_pin):
 
     return distance
 
-def calculate_speed_of_obstacle():
-    return None
+def calculate_speed_of_obstacle(trig_pin, echo_pin):
+    distance1 = measure_distance(trig_pin, echo_pin)
+    
+    time.sleep(SPEED_CALCULATION_INTERVAL)
+    
+    distance2 = measure_distance(trig_pin, echo_pin)
+    
+    diff = distance2 - distance1
+    
+    speed = diff / SPEED_CALCULATION_INTERVAL
+    
+    return speed
+
+class UltrasonicSensor:
+    trig_pin = None
+    echo_pin = None
+    
+    def __init__(self, trig_pin: int, echo_pin: int) -> None:
+        self.trig_pin = trig_pin
+        self.echo_pin = echo_pin
+        
+        GPIO.setup(self.trig_pin, GPIO.OUT)
+        GPIO.setup(self.echo_pin, GPIO.IN)
+        
+    def get_distance(self):
+        return measure_distance(self.trig_pin, self.echo_pin)
+    
+    def get_speed(self):
+        return calculate_speed_of_obstacle(self.trig_pin, self.echo_pin)
+    
+    def __del__(self):
+        GPIO.cleanup(self.trig_pin)
+        GPIO.cleanup(self.echo_pin)
+
+class UltrasonicSensorsModule:
+    front_sensor = None
+    back_sensor = None
+    left_sensor = None
+    right_sensor = None
+    
+    def __init__(self, trig_pins: List[int], echo_pins: List[int]) -> None:
+        self.front_sensor = UltrasonicSensor(trig_pins[0], echo_pins[0])
+        self.back_sensor = UltrasonicSensor(trig_pins[1], echo_pins[1])
+        self.left_sensor = UltrasonicSensor(trig_pins[2], echo_pins[2])
+        self.right_sensor = UltrasonicSensor(trig_pins[3], echo_pins[3])
+        
+    def monitor_for_nearby_obstacles(self) -> None:
+        while True:
+            for i, sensor in enumerate([self.front_sensor, self.back_sensor, self.left_sensor, self.right_sensor]):
+                distance = sensor.get_distance()
+                direction = DIRECTION[i]
+                
+                if distance < DISTANCE_THRESHOLD:
+                    speed = sensor.get_speed()
+                    
+                    if abs(speed) < SPEED_ACCEPTANCE_RANGE: # The obstacle is stationary
+                        continue
+                    elif speed < 0: # The obstacle is moving away from the person
+                        continue
+                    else: # The obstacle is moving towards the person
+                        if distance < ABSOLUTE_DISTANCE_THRESHOLD or speed > SPEED_THRESHOLD:
+                            if direction == "Front":
+                                # Todo: Get 2 images and send to server
+                                pass
+                            else:
+                                print(f"Moving Obstacle detected at {direction}! Watch Out!")
+                            break
+            time.sleep(1)
+            
+"""
+CASES:
+    1: No obstacle detected -> Just Continue
+    2: An obstacle detected within the threshold
+        2.1: The Speed of the obstacle -ve(meaning its moving opposite to the direction of the person) -> Just Continue
+        2.2: The Speed of the obstacle +ve(meaning its moving towards the person)
+            2.2.1: The distance between the person and the obstacle is increasing -> Just Continue
+            2.2.2: The distance between the person and the obstacle is decreasing -> Stop and Alert the person
+            
+TIME COMPLEXITY:
+    Calculate distance = approx (0.001) seconds
+    Get speed = approx (0.001) * 2 + 0.5 seconds
+    
+    
+"""
